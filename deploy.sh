778dfea4b631cc0ddae9a1c54459fd4ebc209775
odvinc <odvinc@gmail.com>
2016-03-16 16:11:43 +0300

    rebuilding site 16 мар 2016 г. 16:11:37

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Remove the public folder to make room for the gh-pages subtree
+rm -rf public/*
+
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add -A
 
@@ -17,8 +21,17 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push to build repos.
 git push origin master
 
 # Come Back
 cd ..
+
+# Add changes to git.
+git add -A
+
+# Commit changes to source code
+git commit -m "$msg"
+
+# Push to source repos.
+git push origin master 
